acc7f168fe89c8aab250b649990bdf6d89458809
s-saito <s.s.saito108@gmail.com>
2023-10-01 04:44:25 +0900

    add file

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # localhost:3000/posts
+  resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
 end
